470c93845ba473569cfb081005ba71233ddbcd12
Tehila <tehilaschechter@gmail.com>
2019-09-23 16:15:30 +0300

    more child work

--- app/src/main/java/com/example/samplescreen/MainActivity.java
+++ app/src/main/java/com/example/samplescreen/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.samplescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnFamily;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFamily = (Button)findViewById(R.id.btnFamily);
+
+       btnFamily.setOnClickListener(new View.OnClickListener(){
+           Intent intNextActivity = new Intent(MainActivity.this, child.class);
+
+
+           @Override
+           public void onClick(View v) {
+                startActivity(intNextActivity);
+           }
+       });
     }
+
+
 }
