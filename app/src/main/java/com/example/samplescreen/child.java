e15c256af4f2580fef969d723752072a5b387a60
Tehila <tehilaschechter@gmail.com>
2019-09-23 16:29:08 +0300

    good gradient option

---
+++ app/src/main/java/com/example/samplescreen/child.java
@@ -0,0 +1,44 @@
+package com.example.samplescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.PaintDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.graphics.drawable.shapes.RectShape;
+import android.os.Bundle;
+import android.view.View;
+
+public class child extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        GradientDrawable gradientDrawable = new GradientDrawable(
+
+                GradientDrawable.Orientation.TOP_BOTTOM,
+                new int[]{ContextCompat.getColor(this, R.color.color_mediaGradientTop),
+                        ContextCompat.getColor(this, R.color.color_mediaGradientSecond),
+                        ContextCompat.getColor(this, R.color.color_mediaGradientThird),
+                        ContextCompat.getColor(this, R.color.color_mediaGradientThird),
+                        ContextCompat.getColor(this,R.color.colorWhite),
+                        ContextCompat.getColor(this,R.color.colorWhite),
+                        ContextCompat.getColor(this, R.color.color_mediaGradientBottom),
+                        ContextCompat.getColor(this, R.color.color_mediaGradientBottom),
+                        ContextCompat.getColor(this, R.color.color_mediaGradientBottom),
+                        ContextCompat.getColor(this, R.color.color_mediaGradientBottom)}
+                );
+
+
+        findViewById(R.id.lytConstraint).setBackground(gradientDrawable);
+
+
+    }
+}
