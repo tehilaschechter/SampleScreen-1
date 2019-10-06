d89142de2ec62550a831fdded8085abb449e98f0
Tehila <tehilaschechter@gmail.com>
2019-10-06 08:39:30 +0300

    family UI

--- app/src/main/java/com/example/samplescreen/Family.java
+++ app/src/main/java/com/example/samplescreen/Family.java
@@ -2,13 +2,29 @@ package com.example.samplescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Family extends AppCompatActivity {
+    Button btnChildren;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        btnChildren = (Button)findViewById(btnChildren);
+
+        btnChildren.setOnClickListener(new View.OnClickListener(){
+            Intent intNextActivity = new Intent(Family.this, Child.class);
+
+
+            @Override
+            public void onClick(View v) {
+                startActivity(intNextActivity);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/samplescreen/MainActivity.java
+++ app/src/main/java/com/example/samplescreen/MainActivity.java
@@ -16,10 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnFamily = (Button)findViewById(R.id.btnFamily);
+       btnFamily = (Button)findViewById(R.id.btnFamily);
 
        btnFamily.setOnClickListener(new View.OnClickListener(){
-           Intent intNextActivity = new Intent(MainActivity.this, Child.class);
+           Intent intNextActivity = new Intent(MainActivity.this, Family.class);
 
 
            @Override
