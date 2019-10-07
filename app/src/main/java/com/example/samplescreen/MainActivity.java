fd7a105eda8f87f8e9f467d9695f1e3e783c2bb9
Tehila <tehilaschechter@gmail.com>
2019-10-07 18:29:06 +0300

    constrain parents, add wisdom activity

--- app/src/main/java/com/example/samplescreen/MainActivity.java
+++ app/src/main/java/com/example/samplescreen/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnChildhood;
     Button btnFamily;
     Button btnWork;
+    Button btnWisdom;
 
 
     @Override
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         btnChildhood = (Button)findViewById(R.id.btnChildhood);
         btnFamily = (Button)findViewById(R.id.btnFamily);
         btnWork = (Button)findViewById(R.id.btnWork);
+        btnWisdom = (Button)findViewById(R.id.btnWisdom);
 
         btnChildhood.setOnClickListener(new View.OnClickListener(){
             Intent intNextActivity = new Intent(MainActivity.this, Childhood.class);
@@ -43,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
 
         btnWork.setOnClickListener(new View.OnClickListener(){
             Intent intNextActivity = new Intent(MainActivity.this, Work.class);
+            @Override
+            public void onClick(View v) {
+                startActivity(intNextActivity);
+                overridePendingTransition(R.transition.anim_cut,R.transition.anim_cut);
+            }
+        });
+
+        btnWisdom.setOnClickListener(new View.OnClickListener(){
+            Intent intNextActivity = new Intent(MainActivity.this, Wisdom.class);
             @Override
             public void onClick(View v) {
                 startActivity(intNextActivity);
