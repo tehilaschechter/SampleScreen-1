150f360a31839018c00fe289a6c2d9a41af31d84
Tehila <tehilaschechter@gmail.com>
2019-10-07 17:01:39 +0300

    childhood layout

--- app/src/main/java/com/example/samplescreen/Family.java
+++ app/src/main/java/com/example/samplescreen/Family.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class Family extends AppCompatActivity {
+    Button btnSpouse;
     Button btnChildren;
     Button btnParents;
 
@@ -16,8 +17,18 @@ public class Family extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
 
+        btnSpouse = (Button)findViewById(R.id.btnSpouse);
         btnChildren = (Button)findViewById(R.id.btnChildren);
         btnParents = (Button)findViewById(R.id.btnParents);
+
+        btnSpouse.setOnClickListener(new View.OnClickListener(){
+            Intent intNextActivity = new Intent(Family.this, Spouse.class);
+            @Override
+            public void onClick(View v) {
+                startActivity(intNextActivity);
+                overridePendingTransition(R.transition.anim_cut,R.transition.anim_cut);
+            }
+        });
 
         btnChildren.setOnClickListener(new View.OnClickListener(){
             Intent intNextActivity = new Intent(Family.this, Child.class);
--- app/src/main/java/com/example/samplescreen/Spouse.java
+++ app/src/main/java/com/example/samplescreen/Spouse.java
@@ -1,14 +1,32 @@
 package com.example.samplescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class Spouse extends AppCompatActivity {
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spouse);
+
+        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
+
+
+        // Programatically set CircleImage background color
+        Drawable background = frameLayout.getBackground();
+        GradientDrawable gradientDrawable = (GradientDrawable) background;
+        gradientDrawable.setColor(ContextCompat.getColor(this,R.color.color_female)); //TODO: CANGE COLOR
+
     }
 }
