159422d60be5e0cba4b445827a3b5dbf24760efa
Tehila <tehilaschechter@gmail.com>
2019-10-09 23:37:42 +0300

    add Parent screens

---
+++ app/src/main/java/com/example/samplescreen/Parent.java
@@ -0,0 +1,14 @@
+package com.example.samplescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Parent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent);
+    }
+}
